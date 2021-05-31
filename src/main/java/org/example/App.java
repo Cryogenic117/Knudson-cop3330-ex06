ae0b1b0e4b0cd2fb8afc93f6e0dd5c050e6405e1
Brandon Knudson <bknudson117@gmail.com>
2021-05-31 14:21:25 -0400

    Commit 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,32 @@
 package org.example;
+import java.time.Year;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Scanner input = new Scanner(System.in);
+        String userInput;
+        Integer age, retireAge, result, year;
+
+        year = Year.now().getValue();
+
+        System.out.println("What is your current age? ");
+        userInput = input.nextLine();
+        age = Integer.parseInt(userInput);
+
+        System.out.println("At what age would you like to retire? ");
+        userInput = input.nextLine();
+        retireAge= Integer.parseInt(userInput);
+        result = retireAge - age;
+        if(result>0) {
+            System.out.printf("You have %d years left until you can retire.\n", result);
+            System.out.printf("It's %d, so you can retire in %d.", year, year+result);
+        }
+        else{
+            System.out.println("You can already retire");
+        }
+
+
     }
 }
